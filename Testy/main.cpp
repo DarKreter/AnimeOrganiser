52a7253382a2cd37a38176fb278a58396e7acd3f
DarKreter <darkreter@gmail.com>
2022-12-27 14:33:07 +0100

    Start refactoring of FilesManagment | done FilesFilter

---
+++ Testy/main.cpp
@@ -0,0 +1,16 @@
+#include "FilesManagment.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    // vector<string> exts = {"cpp", "www", ""};
+    // wstring file = L"123.www";
+
+    // cout << fm::FilesFilter(file, exts) << endl;
+
+    return 0;
+}
