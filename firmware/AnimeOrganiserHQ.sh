8c1346885615d64d0de562dcbae37641aab37513
DarKreter <darkreter@gmail.com>
2022-12-28 20:54:52 +0100

    Add shell scripts to easy change path

---
+++ firmware/AnimeOrganiserHQ.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" ]  
+    then
+    cd "$1"
+fi
+
+/home/kretes/k8d.server.anime_organiser/Execute\ Files/AnimeOrganiserHQ\ v.3
---
+++ firmware/AnimeOrganiserLQ.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+if [ "$1" ]  
+    then
+    cd "$1"
+fi
+
+/home/kretes/k8d.server.anime_organiser/Execute\ Files/AnimeOrganiserLQ\ v.3
